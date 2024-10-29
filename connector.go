ad7404e328cd2d79d26d4c8d51f5ad4d5dda1268
Svetlin Ralchev <iamralch@users.noreply.github.com>
2024-10-29 14:09:01 +0400

    Improve the connector performance
    Run a go-routine that will keep the token valid by refreshing it every
    10 minutes.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- connector.go
+++ connector.go
@@ -2,50 +2,121 @@ package pgxaws
 
 import (
 	"context"
+	"slices"
 	"strconv"
+	"sync/atomic"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/rds/auth"
+	"github.com/aws/smithy-go/logging"
 	"github.com/jackc/pgx/v5"
 )
 
 // Connector connects the the pgx to AWS RDS.
 type Connector struct {
-	// Options to configure the AWS session
-	Options []func(*config.LoadOptions) error
+	// close is the close function.
+	close func()
+	// token is the bearer token.
+	token atomic.Pointer[string]
+	// config is the AWS configuration.
+	config aws.Config
+}
+
+// Connect creates a new connector.
+func Connect(ctx context.Context, options ...func(*config.LoadOptions) error) (*Connector, error) {
+	// prepare the logger
+	WithLogger := func(opt *config.LoadOptions) error {
+		opt.Logger = logging.Nop{}
+		return nil
+	}
+	// prepare the default options
+	options = slices.Insert(options, 0, WithLogger)
+	// prepare the AWS settings
+	config, err := config.LoadDefaultConfig(ctx, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Connector{config: config}, nil
 }
 
 // BeforeConnect is called before a new connection is made. It is passed a copy of the underlying pgx.ConnConfig and
 // will not impact any existing open connections.
-func (x *Connector) BeforeConnect(ctx context.Context, conn *pgx.ConnConfig) error {
+func (x *Connector) BeforeConnect(ctx context.Context, config *pgx.ConnConfig) (err error) {
 	// if there is no user, we can't issue a token
-	if conn.User == "" {
-		return nil
-	}
-
-	// prepare the AWS settings
-	settings, err := config.LoadDefaultConfig(ctx, x.Options...)
-	if err != nil {
-		return err
+	if config.User == "" {
+		x.config.Logger.Logf(logging.Debug, "no user set")
+		return
 	}
 
 	// if there is no region, we can't issue a token
-	if settings.Region == "" {
-		return nil
+	if x.config.Region == "" {
+		x.config.Logger.Logf(logging.Debug, "no region set")
+		return
 	}
 
-	// issue the token
-	token, err := auth.BuildAuthToken(ctx, x.endpoint(conn), settings.Region, conn.User, settings.Credentials)
-	if err != nil {
-		return err
+	token := x.token.Load()
+	// we should authorize the token
+	if token == nil {
+		// authorize
+		if token, err = x.authorize(ctx, config); err != nil {
+			x.config.Logger.Logf(logging.Debug, err.Error())
+			return err
+		}
+		// set the token
+		x.token.Store(token)
+		// prepare the Background context
+		ctx = context.Background()
+		ctx, x.close = context.WithCancel(ctx)
+		// refresh the token
+		go x.session(ctx, config)
 	}
 
 	// set the token as password
-	conn.Password = token
+	config.Password = *token
 	// done!
 	return nil
 }
 
-func (x *Connector) endpoint(conn *pgx.ConnConfig) string {
-	return conn.Host + ":" + strconv.Itoa(int(conn.Port))
+// Close closes the connector.
+func (x *Connector) Close() {
+	if x.close == nil {
+		return
+	}
+
+	x.close()
+}
+
+// session refreshes the token.
+func (x *Connector) session(ctx context.Context, config *pgx.ConnConfig) {
+	ticker := time.NewTicker(10 * time.Minute)
+
+	for {
+		select {
+		case <-ticker.C:
+			token, err := x.authorize(ctx, config)
+			if err != nil {
+				x.config.Logger.Logf(logging.Warn, err.Error())
+			}
+			// store the token
+			x.token.Store(token)
+		case <-ctx.Done():
+			x.token.Store(nil)
+			x.close = nil
+			return
+		}
+	}
+}
+
+func (x *Connector) authorize(ctx context.Context, config *pgx.ConnConfig) (*string, error) {
+	endpoint := config.Host + ":" + strconv.Itoa(int(config.Port))
+	// build token
+	token, err := auth.BuildAuthToken(ctx, endpoint, x.config.Region, config.User, x.config.Credentials)
+	if err != nil {
+		return nil, err
+	}
+
+	return &token, nil
 }
--- connector_test.go
+++ connector_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -17,23 +16,26 @@ func ExampleConnector() {
 		panic(err)
 	}
 
-	// Set max connection lifetime to 5 minutes in postgres connection pool configuration.
-	// Note: this will refresh connections before the 15 min expiration on the IAM AWS auth token,
-	// while leveraging the BeforeConnect hook to recreate the token in time dynamically.
-	config.MaxConnLifetime = 5 * time.Minute
-
+	ctx := context.TODO()
 	// Create a new pgxaws.Connector
-	connector := &pgxaws.Connector{}
-	// Set BeforeConnect hook to pgxaws.BeforeConnect
-	config.BeforeConnect = connector.BeforeConnect
-
-	// Create a new pgxpool with the config
-	conn, err := pgxpool.NewWithConfig(context.TODO(), config)
+	connector, err := pgxaws.Connect(ctx)
 	if err != nil {
 		panic(err)
 	}
+	// close the connector
+	defer connector.Close()
 
-	rows, err := conn.Query(context.TODO(), "SELECT * from organization")
+	config.BeforeConnect = connector.BeforeConnect
+
+	// Create a new pgxpool with the config
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		panic(err)
+	}
+	// close the pool
+	defer pool.Close()
+
+	rows, err := pool.Query(ctx, "SELECT * from organization")
 	if err != nil {
 		panic(err)
 	}
